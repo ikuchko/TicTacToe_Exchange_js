15165ace8c0912b51d591106b0137253061687bc
Gregory K and Mike Mahoney <gregkanteres@gmail.com>
2016-02-10 08:33:05 -0800

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+function Player(mark) {
+  this.mark = mark;
+}
+
+function Space(x_coordinate, y_coordinate) {
+  this.x_coordinate = x_coordinate;
+  this.y_coordinate = y_coordinate;
+}
+
+function Board() {
+
+}
+
+function Game() {
+
+}
