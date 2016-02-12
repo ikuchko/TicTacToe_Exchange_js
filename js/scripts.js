d81ae928674259f8e85af5f8571f0c6dea0a1427
Illia Kuchko and Kassidy Douglas <ikuchko@gmail.com>
2016-02-11 16:43:36 -0800

    Add multiplayer and single player capability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,232 @@
-function Player(t_mark) {
-  this.t_mark = t_mark;
+function Player(mark, isAI) {
+  this.mark = mark;
+  this.isAI = isAI;
 }
 
-Player.prototype.mark = function() {
-  return this.t_mark;
+Player.prototype.markSpace = function(space) {
+  if (space.player === undefined) {
+    space.player = this;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
-function Space(x_coordinate, y_coordinate) {
-  this.x_coordinate = x_coordinate;
-  this.y_coordinate = y_coordinate;
-  this.player = "";
+function Space(xCoordinate, yCoordinate) {
+  this.xCoordinate = xCoordinate;
+  this.yCoordinate = yCoordinate;
+  this.player;
 }
 
-Space.prototype.markedBy = function(player) {
- return this.player = player;
+Space.prototype.markedBy = function() {
+ return this.player;
 }
 
 
 function Board() {
   this.spaces = [];
-}
-
-Board.prototype.initialize = function() {
-  z = 0;
   for (var i = 1; i <= 3; i += 1) {
-      for (var j = 1; j <= 3; j += 1) {
-        var new_space = new Space(i, j)
-        this.spaces.push(new_space);
-        if (i != z) {
-          $("table").append('<tr id=row' + i + '></tr>');
-          z = i;
-        }
-        $('tr#row' + i).append('<td id=' + i + ',' + j +'></td>');
-      }
-    };
+    for (var j = 1; j <= 3; j += 1) {
+      var newSpace = new Space(i, j)
+      this.spaces.push(newSpace);
+    }
+  }
 }
 
 
-Board.prototype.threeInARow = function() {
+Board.prototype.findSpace = function(xCoordinate, yCoordinate) {
+  for(var indvSpace in this.spaces) {
+    if ((xCoordinate === this.spaces[indvSpace].xCoordinate) && (yCoordinate === this.spaces[indvSpace].yCoordinate)){
+      return this.spaces[indvSpace];
+    }
+  }
 }
 
-function Game(playerOneName, playerTwoName) {
 
+function Game(multiplayer) {
+  this.multiplayer = multiplayer;
+  this.players = [];
+  this.currentTurn = 0;
+  //this.currentTurn = Math.floor(Math.random() * 2);
+  this.board = new Board();
+
+  this.players.push(new Player("X", false));
+  if(multiplayer === true) {
+    this.players.push(new Player("O", false));
+  } else {
+    this.players.push(new Player("O", true));
+  }
 }
+
+Game.prototype.aiTurn = function(level) {
+  var xCoordinate;
+  var yCoordinate;
+  if (this.countMarkSpaces() < 9) {
+    if (level === 0) {
+      do {
+        xCoordinate = Math.floor((Math.random() * 3) +1);
+        yCoordinate = Math.floor((Math.random() * 3) +1);
+      } while (this.players[1].markSpace(this.board.findSpace(xCoordinate, yCoordinate)) === false);
+    }
+    this.changeTurn();
+    return xCoordinate + "-" + yCoordinate;
+  }
+}
+
+Game.prototype.changeTurn = function () {
+  this.currentTurn = (this.currentTurn === 0)? 1: 0;
+}
+
+Game.prototype.countMarkSpaces = function() {
+  var count = 0;
+  for (var index in this.board.spaces) {
+    if (this.board.spaces[index].player !== undefined) {
+      count ++;
+    }
+  }
+  return count;
+}
+
+Game.prototype.checkDiagonal = function () {
+  if (this.board.findSpace(2, 2).markedBy() != undefined) {
+    if (((this.board.findSpace(1, 1).markedBy() === this.board.findSpace(2, 2).markedBy())  && (this.board.findSpace(1, 1).markedBy() === this.board.findSpace(3, 3).markedBy())) || ((this.board.findSpace(1, 3).markedBy() === this.board.findSpace(2, 2).markedBy())  && (this.board.findSpace(1, 3).markedBy() === this.board.findSpace(3, 1).markedBy()))) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+Game.prototype.checkVertical = function () {
+  // debugger;
+  if (this.board.findSpace(2, 1).markedBy() != undefined) {
+    if ((this.board.findSpace(1, 1).markedBy() === this.board.findSpace(2, 1).markedBy())  && (this.board.findSpace(2, 1).markedBy() === this.board.findSpace(3, 1).markedBy()) &&
+    (this.board.findSpace(1, 1).markedBy() === this.board.findSpace(3, 1).markedBy())) {
+      return true;
+    }
+  }
+
+  if (this.board.findSpace(2, 2).markedBy() != undefined) {
+    if ((this.board.findSpace(1, 2).markedBy() === this.board.findSpace(2, 2).markedBy())  && (this.board.findSpace(2, 2).markedBy() === this.board.findSpace(3, 2).markedBy()) &&
+    (this.board.findSpace(1, 2).markedBy() === this.board.findSpace(3, 2).markedBy())) {
+      return true;
+    }
+  }
+
+  if (this.board.findSpace(2, 3).markedBy() != undefined) {
+    if ((this.board.findSpace(1, 3).markedBy() === this.board.findSpace(2, 3).markedBy()) &&      (this.board.findSpace(1, 3).markedBy() === this.board.findSpace(3, 3).markedBy())  && (this.board.findSpace(2, 3).markedBy() === this.board.findSpace(3, 3).markedBy())) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+Game.prototype.checkHorizontal = function () {
+  if (this.board.findSpace(1, 2).markedBy() != undefined) {
+    if ((this.board.findSpace(1, 1).markedBy() === this.board.findSpace(1, 2).markedBy())  && (this.board.findSpace(1, 2).markedBy() === this.board.findSpace(1, 3).markedBy()) &&
+    (this.board.findSpace(1, 1).markedBy() === this.board.findSpace(1, 3).markedBy())) {
+      return true;
+    }
+  }
+
+  if (this.board.findSpace(2, 2).markedBy() != undefined) {
+    if ((this.board.findSpace(2, 1).markedBy() === this.board.findSpace(2, 2).markedBy())  && (this.board.findSpace(2, 2).markedBy() === this.board.findSpace(2, 3).markedBy()) &&
+    (this.board.findSpace(2, 1).markedBy() === this.board.findSpace(2, 3).markedBy())) {
+      return true;
+    }
+  }
+
+  if (this.board.findSpace(3, 2).markedBy() != undefined) {
+    if ((this.board.findSpace(3, 1).markedBy() === this.board.findSpace(3, 2).markedBy()) &&      (this.board.findSpace(3, 1).markedBy() === this.board.findSpace(3, 3).markedBy())  && (this.board.findSpace(3, 2).markedBy() === this.board.findSpace(3, 3).markedBy())) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+Game.prototype.findWinner = function() {
+  debugger;
+  if (this.checkVertical() || this.checkHorizontal() || this.checkDiagonal()) {
+    return this.players[this.currentTurn];
+  } else if (this.countMarkSpaces() === 9) {
+    return false;
+  }
+}
+
+Game.prototype.takeTurn = function (x, y) {
+  var space = this.board.findSpace(x, y);
+  var playerTurn = this.currentTurn;
+  //debugger;
+  if(this.players[this.currentTurn].markSpace(space)) {
+    //this.populateTable();
+    return this.players[playerTurn].mark;
+  }
+}
+
+Game.prototype.populateTable = function () {
+  for (var index in this.board.spaces) {
+    $("#" + this.board.spaces[index].xCoordinate + this.board.spaces[index].yCoordinate).text(this.board.spaces[index].player);
+  }
+};
 
 $(document).ready(function() {
-  $("form#new-game").submit(function(event) {
-
-    $("table").on("click", "td", function() {
-      console.log(this);
-      //board.find(--location).markedBy(currentPlayer);
-      //$(this).text(player.mark) mark td with player X or O
-
-    });
-
+  $("form#single-player").submit(function(event) {
     event.preventDefault();
 
-    new_board = new Board();
-    new_board.initialize();
-    console.log("Lets play")
-    console.log(new_board)
+    var game = new Game(false);
+    game.populateTable();
+
+
+    $("td").click(function(event) {
+      var xCoordinate = parseInt(((event.target.id).split("-")[0]));
+      var yCoordinate = parseInt(((event.target.id).split("-")[1]));
+      var player = game.takeTurn(xCoordinate, yCoordinate);
+      if (player != undefined) {
+        $("#" + event.target.id).append("<h1>" + "X" + "</h1>");
+        var coordinates = game.aiTurn(0);
+        $("#" + coordinates).append("<h1>" + "O" + "</h1>");
+      }
+      //debugger;
+      if (game.findWinner() === undefined) {
+        game.changeTurn();
+      } else if (game.findWinner() != undefined) {
+        var player = game.findWinner();
+        var playerWin = player.mark
+        $(".winner").show();
+        if (player === false){
+          $(".winner").append("<h1>Draw!!</h1>");
+        } else if(player.mark === "X") {
+          $(".winner").append("<h1>You beat the computer!</h1>");
+        } else if (player.mark === "O"){
+          $(".winner").append("<h1>Computer won!</h1>");
+        }
+      }
+    });
+  });
+
+  $("form#multiplayer").submit(function(event) {
+    event.preventDefault();
+
+    var game = new Game(true);
+    game.populateTable();
+
+    $("td").click(function(event) {
+      var xCoordinate = parseInt(((event.target.id).split("-")[0]));
+      var yCoordinate = parseInt(((event.target.id).split("-")[1]));
+      // console.log(game.takeTurn(xCoordinate, yCoordinate));
+      $("#" + event.target.id).append("<h1>" + game.takeTurn(xCoordinate, yCoordinate) + "</h1>");
+
+      if (game.findWinner() != undefined) {
+        var playerWin = game.findWinner().mark
+        $(".winner").show();
+      }
+    });
+  });
+
+  $("form#play-again").submit(function(event) {
+    location.reload(forceGet);
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,33 +1,129 @@
 describe('Player', function() {
     it("returns the player's mark", function() {
-      var testPlayer = new Player("X");
-      expect(testPlayer.mark()).to.equal("X");
+      var space = new Space(1,2);
+      var testPlayer = new Player("X", true);
+      testPlayer.markSpace(space);
+      expect(space.player).to.equal(testPlayer);
    });
+
+   it("does not allow two players to mark the same space", function() {
+     var testPlayer = new Player("X", true);
+     var secondTestPlayer = new Player("O", true);
+     var testSpace = new Space(1,2);
+     testPlayer.markSpace(testSpace);
+     secondTestPlayer.markSpace(testSpace);
+     expect(testSpace.player).to.equal(testPlayer);
+  });
 });
 
 describe('Space', function() {
     it("returns the player's mark", function() {
       var testSpace = new Space(1,2);
-      expect(testSpace.x_coordinate).to.equal(1);
+      expect(testSpace.xCoordinate).to.equal(1);
    });
 
    it("returns the player's mark", function() {
      var testSpace = new Space(1,2);
-     expect(testSpace.y_coordinate).to.equal(2);
+     expect(testSpace.yCoordinate).to.equal(2);
    });
 
   it("lets a player mark a space", function() {
-      var testPlayer = new Player("X")
+      var testPlayer = new Player("X", true);
       var testSpace = new Space(1,2);
-      testSpace.markedBy(testPlayer)
-      expect(testSpace.player).to.equal(testPlayer);
+      testPlayer.markSpace(testSpace);
+      expect(testSpace.markedBy()).to.equal(testPlayer);
    });
 });
 
 describe('Board', function() {
   it("creates 9 spaces when it is initialized", function() {
     var testBoard = new Board();
-    testBoard.initialize();
-    expect(testBoard.spaces[0].x_coordinate).to.equal(1);
+    expect(testBoard.spaces[8].xCoordinate).to.equal(3);
    });
+
+   it("returns space on the board by coordinates", function() {
+     var testBoard = new Board();
+     var testSpace = testBoard.findSpace(3,2);
+     expect(testSpace.xCoordinate).to.equal(3);
+     expect(testSpace.yCoordinate).to.equal(2);
+    });
  });
+
+ describe('Game', function() {
+  it("checks all constructor variables", function(){
+    var game = new Game(true);
+    expect(game.multiplayer).to.equal(true);
+    expect(game.players[0].isAI).to.equal(false);
+    expect(game.currentTurn < 2).to.equal(!undefined);
+  });
+
+  it("changeTurn method changes player turn", function() {
+    var game = new Game(true);
+    var previousTurn = game.currentTurn;
+    expect(game.changeTurn()).to.not.equal(previousTurn);
+  });
+
+  it("winner method returns winning player for horizontal match", function() {
+    var game = new Game(true);
+    var board = game.board;
+    var player = game.players[0];
+    player.markSpace(board.findSpace(3, 1));
+    player.markSpace(board.findSpace(3, 2));
+    player.markSpace(board.findSpace(3, 3));
+    expect(game.findWinner()).to.equal(player);
+  });
+
+  it("winner method returns winning player for vertical match", function() {
+    var game = new Game(true);
+    var board = game.board;
+    var player = game.players[0];
+    player.markSpace(board.findSpace(1, 1));
+    player.markSpace(board.findSpace(2, 1));
+    player.markSpace(board.findSpace(3, 1));
+    // debugger;
+    expect(game.findWinner()).to.equal(player);
+  });
+
+  it("winner method returns winning player for diagonal match", function() {
+    var game = new Game(true);
+    //var board = game.board;
+    var player = game.players[0];
+    player.markSpace(board.findSpace(1, 1));
+    player.markSpace(board.findSpace(2, 2));
+    player.markSpace(board.findSpace(3, 3));
+    expect(game.findWinner()).to.equal(player);
+  });
+
+  it("winner method returns winning player for next combination", function() {
+    var game = new Game(true);
+    var board = game.board;
+    var player1 = game.players[0];
+    var player2 = game.players[1];
+    player1.markSpace(board.findSpace(1, 3));
+    player2.markSpace(board.findSpace(1, 2));
+    expect(game.findWinner()).to.equal(undefined);
+  });
+
+  it("winner method returns winning player for next combination", function() {
+    var game = new Game(true);
+    var board = game.board;
+    var player1 = game.players[0];
+    var player2 = game.players[1];
+    player1.markSpace(board.findSpace(1, 1));
+    player1.markSpace(board.findSpace(1, 3));
+    player1.markSpace(board.findSpace(2, 3));
+    player2.markSpace(board.findSpace(1, 2));
+    player2.markSpace(board.findSpace(2, 1));
+    expect(game.findWinner()).to.equal(undefined);
+  });
+
+  it("marks space on AI turn", function() {
+    var game = new Game(false);
+    var board = game.board;
+    var player1 = game.players[0];
+    player1.markSpace(board.findSpace(1, 1));
+    game.aiTurn(0);
+    expect(game.countMarkSpaces()).to.equal(2);
+  });
+
+});
